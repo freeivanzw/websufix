c35a9466034f327cc9fda1a37f6954ad421fc241
Ivan Kozak <freeivanzw@gmail.com>
2022-07-08 16:50:00 +0300

    header footer banner

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,79 @@
-console.log('hello world1')
+$(function () {
+    jQuery.fn.dropdown = function (options) {
 
-$('.test_slider').slick()
+        var settings = $.extend({
+            arrow: '',
+            prevent: true,
+            onChange: ''
+        }, options);
+
+        return this.each(function () {
+            var $this = $(this);
+
+            if ($this.find('.selected').length > 0) {
+                $this.find('span:eq(0)')
+                    .html($this.find('.selected').text() + settings.arrow)
+                    .end()
+                    .find('input').val($this.find('.selected a').data('value'))
+                    .end()
+                    .find('.selected').closest('li').hide();
+            }
+
+            $this.on('click', '.overflow', function (e) {
+                e.preventDefault();
+
+                if (!$(this).closest('.dropdown').hasClass('dropdown-open')) {
+                    $.when($('.dropdown').each(function () {
+                        $(this).removeClass('dropdown-open').find('ul:eq(0)').slideUp();
+                    })).then(function () {
+                        $this.addClass('dropdown-open').find('ul:eq(0)').slideDown(function () {
+                            var h = parseInt($(this).outerHeight(true, true)),
+                                top = parseInt($(this).offset()['top']) - parseInt($(document).scrollTop()),
+                                wh = parseInt($(window).height());
+
+                            if (top + h > wh) {
+                                $(this).css({
+                                    'max-height': wh - top - 10
+                                });
+                            } else {
+                                $(this).css({
+                                    'max-height': 'auto'
+                                });
+                            }
+                        });
+                    });
+                } else {
+                    $this.removeClass('dropdown-open').find('ul:eq(0)').slideUp();
+                }
+            });
+
+            $this.find('ul').eq(0).on('click', 'a', function (e) {
+                e.preventDefault();
+
+                if (!$(this).hasClass('disabled')) {
+                    if (settings.prevent === false) {
+                        window.location.href = $(this).attr('href');
+                    } else {
+                        $(this).closest('ul').find('.selected').removeClass('selected').show().end().end().closest('li').addClass('selected').hide();
+                        $this.find('span:eq(0)').html($(this).text() + settings.arrow).end().find('input').val($(this).data('value'));
+
+                        $this.removeClass('dropdown-open').find('ul:eq(0)').slideUp();
+                        if ($.isFunction(settings.onChange)) settings.onChange($(this));
+                    }
+                }
+            });
+        });
+    };
+
+    $('.mob_menu-open').on('click', function () {
+        $('.mob_menu').addClass('active')
+    })
+
+    $('.mob_menu-close').on('click', function () {
+        $('.mob_menu').removeClass('active')
+    })
+
+    $('.order_delivery').dropdown();
+
+
+})
